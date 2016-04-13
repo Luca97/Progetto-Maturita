e1c82ea7bb3740bbd0d475eedbe3c54a65f47fb7
lLarryl <vittoriolarovere@live.it>
2016-04-13 12:32:27 +0200

    Added files via upload

---
+++ cambiaEmail/cambiaEmail.php
@@ -0,0 +1,13 @@
+<html>
+	<body>
+		<form id="form1" name="form1" method="post" action="check_cambiaEmail.php">
+			Username :
+			<input name="Username" type="text" id="Username"/>
+			Password Corrente:
+			<input name="Password" type="password" id="Password"/>
+			Nuova Email :
+			<input name="newEmail" type="mail" id="newEmail"/>
+			<input type="submit" name="Submit" value="Submit"/>
+		</form>
+	</body>
+</html>
