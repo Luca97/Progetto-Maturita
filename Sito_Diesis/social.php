f8f494344f02d734d56e2ce4fec873745ac70057
lLarryl <vittoriolarovere@live.it>
2016-04-29 09:51:09 +0200

    Added files via upload

--- Sito_Diesis/social.php
+++ Sito_Diesis/social.php
@@ -13,6 +13,7 @@
 	<!--<link href="style.css" rel="stylesheet">-->
     <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/styleSocial.css" rel="stylesheet">
 	
 	<script type="text/javascript" src="chkLogin.js"></script>
 	<!--<link href="style.css" rel="stylesheet">-->
@@ -25,16 +26,8 @@
         /* Required padding for .navbar-fixed-top. Remove if using .navbar-static-top. Change if height of navigation changes. */
     }
     </style>
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-</head>
-<?php
+	
+	<?php
 	session_start();
 	include "dbClass.php";
 	$db= new dbClass;
@@ -83,7 +76,7 @@
 						<a href="visualizza.php">Files XML</a>
 					</li>
 					<li>
-						<a href="social.php">Social</a>
+						<a href="#">Social</a>
 					</li>
 					<li>
 						<a href="login.php">Logout</a>
@@ -94,48 +87,13 @@
         </div>
         <!-- /.container -->
     </nav>
-
-    <!-- Page Content -->
-    <div class="container">
 	
-	<!--<div id="immagine">
-			<img src="logo.png" style="width:200;">
-		</div>-->
-		<form class="form" action="sezioni.html" method="POST">
-			<h2>Area utente</h2>
-			<table border="1" style="width:100%">
-			<tr><!-- riga -->
-				<th>Username:</th><!-- colonna -->
-				<td><?php echo $ar[0]; ?></td><!-- colonna -->					
-			 </tr><!-- fine riga -->			  
-			<tr><!-- riga -->
-				<th>Email:</th><!-- colonna -->
-				<td><?php echo $ar[1]; ?></td><!-- colonna -->					
-			 </tr><!-- fine riga -->
-			</table>
+	<div class="container">
+		<form id="search" action="chkSocial.php" method="post">
+				<fieldset>
+					<legend>Cerca nel sito</legend>
+					<input type="text" id="search-in" value="cerca nel sito"> 
+					<button><img id="imgSearch" src="search1.png" alt="Cerca" title="Avvia la ricerca"></button>
+				</fieldset>
 		</form>
-	
-       <!-- <div class="row">
-            <div class="col-lg-12 text-center">
-                <h1>A Bootstrap Starter Template</h1>
-                <p class="lead">Complete with pre-defined file paths that you won't have to change!</p>
-                <ul class="list-unstyled">
-                    <li>Bootstrap v3.3.6</li>
-                    <li>jQuery v1.11.1</li>
-                </ul>
-            </div>
-        </div>
-        <!-- /.row -->
-
-    </div>
-    <!-- /.container -->
-
-    <!-- jQuery Version 1.11.1 -->
-    <script src="js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-	
-</body>
-
-</html>
+	</div>
