363dfd2576a7598ff9935dea6f0e12d829348546
whmori17 <gabrielemori@outlook.com>
2016-06-13 13:52:32 +0200

    Sito ultimato

---
+++ sito_diesis/layout/foot.php
@@ -0,0 +1,20 @@
+<div class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="copyright">
+                        <p>© 2015 Copyright by SYK - Share Your Knowledge. All Rights Reserved.</p>
+                    </div>
+                </div>
+                
+                <!--<div class="col-md-4">
+                    <div class="footer-card-icon">
+                        <i class="fa fa-cc-discover"></i>
+                        <i class="fa fa-cc-mastercard"></i>
+                        <i class="fa fa-cc-paypal"></i>
+                        <i class="fa fa-cc-visa"></i>
+                    </div>
+                </div>-->
+            </div>
+        </div>
+    </div>
